f3953b6561c32a7020f30d3eefce0e8084075178
Adrián Antón <adrtler@gmail.com>
2021-12-08 17:08:07 +0100

    Update ShoppingPage.tsx

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,53 +1,23 @@
 import ProductCard from '../components';
 import { products } from '../data/products';
-import { useShoppingCart } from '../hooks/useShoppingCart';
 import '../styles/custom-styles.css';
 
+const product = products[0];
+
 export const ShoppingPage = () => {
-  const { shoppingCart, onProductCountChange } = useShoppingCart();
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
-      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-        {products.map((product) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            className="bg-dark text-white"
-            value={
-              shoppingCart[product.id] ? shoppingCart[product.id].count : 0
-            }
-            onChange={onProductCountChange}
-          >
-            <ProductCard.Image className="custom-image" />
-            <ProductCard.Title className="text-bold" />
-            <ProductCard.Buttons className="custom-buttons" />
-          </ProductCard>
-        ))}
-      </div>
-      <div className="shopping-cart ">
-        {/* también se puede hacer con el .entries */}
-        {Object.keys(shoppingCart).map((index) => (
-          <ProductCard
-            key={index}
-            product={shoppingCart[index]}
-            className="bg-dark text-white"
-            style={{ width: '100px' }}
-            onChange={onProductCountChange}
-            value={shoppingCart[index].count}
-          >
-            <ProductCard.Image className="custom-image" />
-            <ProductCard.Buttons
-              style={{ display: 'flex', justifyContent: 'center' }}
-              className="custom-buttons"
-            />
-          </ProductCard>
-        ))}
-      </div>
-      <div>
-        <code>{JSON.stringify(shoppingCart, null, 5)}</code>
-      </div>
+      <ProductCard
+        key={product.id}
+        product={product}
+        className="bg-dark text-white"
+      >
+        <ProductCard.Image className="custom-image" />
+        <ProductCard.Title className="text-bold" />
+        <ProductCard.Buttons className="custom-buttons" />
+      </ProductCard>
     </div>
   );
 };
